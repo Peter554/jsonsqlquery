3ea95fc4c321d56f310cf1d8b658599e53b3fc1e
Peter Byfield <byfield554@gmail.com>
2023-03-04 19:59:44 +0100

    package

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
-cat examples/in_data.jsonl | poetry run python main.py --query-file examples/query_1.sql > /tmp/out_data.jsonl
+cat examples/in_data.jsonl | poetry run python jsonquery/main.py --query-file examples/query_1.sql > /tmp/out_data.jsonl
 cmp /tmp/out_data.jsonl examples/out_data_1.jsonl
 
-cat examples/in_data.jsonl | poetry run python main.py --query-file examples/query_2.sql > /tmp/out_data.jsonl
+cat examples/in_data.jsonl | poetry run python jsonquery/main.py --query-file examples/query_2.sql > /tmp/out_data.jsonl
 cmp /tmp/out_data.jsonl examples/out_data_2.jsonl
